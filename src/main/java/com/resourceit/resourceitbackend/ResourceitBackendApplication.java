52542a097efc387da5dbe4cecc0281cc978f5aed
unknown <gferrijr@gmailcom>
2018-04-16 00:48:14 -0300

    Primeiro commit

---
+++ src/main/java/com/resourceit/resourceitbackend/ResourceitBackendApplication.java
@@ -0,0 +1,12 @@
+package com.resourceit.resourceitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceitBackendApplication.class, args);
+	}
+}
